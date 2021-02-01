3280c99ad7eaa94fbe5c43ebb07ec32031d99a6a
Nazarii <nazarrishchuk@gmail.com>
2021-02-01 16:02:41 +0200

    Updates

---
+++ precache-manifest.22606597daf904306711f8158a01da8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379399e0d7315ab8672131f04a82fdfc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e165cbd5b6df3fc319d",
+    "url": "./static/js/main.c49000c7.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.22606597daf904306711f8158a01da8d.js"
 );
 
 self.addEventListener('message', (event) => {
